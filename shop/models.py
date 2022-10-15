88f22d4f0290e0ebac27f7cebe72fbe62ace3147
iskendergithub <iskenmamatov@gmail.com>
2022-10-15 20:27:06 +0600

    updates models, urls, views

--- shop/models.py
+++ shop/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=8, blank=True, null=True)
     quantity = models.PositiveSmallIntegerField()
-    related_shop = models.ManyToManyField(RetailShop)
+    related_shop = models.ManyToManyField(RetailShop, related_name='related_shop')
 
     def __str__(self):
         return self.title
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .serializers import RetailShopSerializer, ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import RetailShop, Product
 
-# Create your views here.
+
+@api_view(['GET'])
+def stores_list_view(request):
+    shops = RetailShop.objects.all()
+    serializer = RetailShopSerializer(shops, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def stores_detail_view(request, id):
+    details = RetailShop.objects.get(id=id)
+    serializer = RetailShopSerializer(details, many=False)
+    return Response(serializer.data)
