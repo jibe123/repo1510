de3596b40fd57d714156954df38e3e532f5d7809
jibe123 <jibek.k@gmail.com>
2022-10-15 18:45:40 +0600

    updated models.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class RetailShop(models.Model):
+    title = models.CharField(max_length=150)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=8, blank=True, null=True)
+    quantity = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.title
