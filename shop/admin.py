ccb1e204b9348e1f4f2d42a0eca8e39fb8bc2fd8
jibe123 <jibek.k@gmail.com>
2022-10-15 19:12:00 +0600

    updated admin.py and urls.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import RetailShop, Product
 
-# Register your models here.
+admin.site.register(RetailShop)
+admin.site.register(Product)
---
+++ shop/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+import shop.views as views
+
+urlpatterns = [
+
+]
